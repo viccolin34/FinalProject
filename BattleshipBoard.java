bdaa06b89bee6f2e1681b08d65d454318cebe668
viccolin34 <45371567+viccolin34@users.noreply.github.com>
2018-12-06 13:44:22 -0600

    Add files via upload

--- BattleshipBoard.java
+++ BattleshipBoard.java
@@ -1,109 +1,174 @@
 import java.util.Scanner;
-/*The BattleshipBoard class contains methods that define the game board and create the game board
- */
-
-
    public class BattleshipBoard
    {
-	   
-	   private static String[][] board;
-	   private int row;
-	   private int col;
-	   
-	   public BattleshipBoard(int row, int col) {
-		   this.row = row;
-		   this.col = col;
-		   board = new String[row][col];
-		   createBoard();
-		   showBoard();
-	   }
-	   public int getCol() {
-		   return col;
-	   }
-	   
-	   public int getRow() {
-		   return row;
-	   }
-	   
-	   public String[][] getBoard(){
-		   return board;
-	   }
+   
+  private static String[][] board; //variables
+  private int row;
+  private int col;
+   
+  public BattleshipBoard(int row, int col) {
+  this.row = row;
+  this.col = col;
+  board = new String[row][col];
+  createBoard();
+  showBoard();
+  }
+  public int getCol() {
+  return col;
+  }
+   
+  public int getRow() {
+  return row;
+  }
+   
+  public String[][] getBoard(){
+  return board;
+  }
      
-      public void createBoard()
-     
+      public void createBoard() //first function creates the board using two for loops 
+      //one for rows one for columns 
       {
-    	 for(int row = 0; row < board.length; row++)
+         for(int row = 0; row < board.length; row++)
          {
             for(int column = 0; column < board[0].length; column++)
             {
-               board[row][column] = "-";
+               board[row][column] = "-"; //board is represented by dash marks
             }
          }
       }
-      public void showBoard()
-      {
+      public void showBoard() { //makes board appear on screen
+
+
+
          for(int row = 0; row < board.length; row++) {
+         
+
+        	 System.out.print((row+1) + " ");
+         for(int column = 0; column < board[0].length; column++) {
         	 
-        	 for(int column = 0; column < board[0].length; column++) {
-        		 
-        		 if(board[row][column].equals("S")) {
-        			 
-        			 System.out.print(" " + "S");
-        		
-        		 } else {
-        			 System.out.print(" " + board[row][column]);
-        		 }
-        	 }
-        	 System.out.println("");
+         
+         if(board[row][column].equals("S")) { //Ship's are labeled with "S" marks
+         
+         System.out.print(" " + "-"); //hides the "S'" with dash marks 
+         
+         } else {
+         
+         System.out.print(" " + board[row][column]);
          }
-    	 System.out.println("");
+         }
+         System.out.println("");
+         }
+     System.out.println("");
 
       }
      
-      
-      public int fireMissles(String[][] battleshipBoard, int missles, int hits, int row, int col) {
-    	  Scanner keyboard = new Scanner(System.in);
-    	  System.out.println("Please enter a row in which you would like to fire upon. 1 through 8.");
-    	  row = keyboard.nextInt();
-    	  while(row > 8 || row < 1){
-    		  System.out.println("Please enter a row in which you would like to fire upon. 1 through 8.");
-    		  row = keyboard.nextInt();
-      }
-    	  System.out.println("Please enter a column in which you would like to fire upon. 1 through 8.");
-    	  col = keyboard.nextInt();
-      while(col > 8 || col < 1 ) {
-    	  System.out.println("Please enter a column in which you would like to fire upon. 1 through 8.");
-    	  col = keyboard.nextInt();
-      }
-      if(board[row-1][col-1].equals("S"))
-          {
-           
-             System.out.println("HIT");
-             board[row-1][col-1] = "!";
-             hits++;
-          }
-          else
-          {
-             System.out.println("MISS");
-             board[row-1][col-1] = "O";
-          }
-          return hits;
-      
-      }
-      
-      
+    
       public static void main(String[] arg) {
-         BattleshipBoard bshipboard = new BattleshipBoard(8,8);
+     String goAgain;
+         Scanner keyboard = new Scanner(System.in);
+         String difficulty;
+         
+         do {
+         
+         System.out.println("Choose your difficulty! Type 'easy' if you're a wuss, "
+         + "'medium' if you're an average joe, or "
+         + "'hard' if you just love to torture yourself");
+         difficulty = keyboard.next();
+         if (difficulty.equalsIgnoreCase("easy") ) { 
+         
+        
+     
+    System.out.println("   1 2 3 4 5 6"); 
+         BattleshipBoard bshipboard = new BattleshipBoard(6,6);
          PositionofShip p = new PositionofShip(bshipboard) ;
          p.createShip(3);
+    
+
+  System.out.println("   1 2 3 4 5 6"); 
+
          bshipboard.showBoard();
-         Player p1 = new Player(bshipboard);
-         p1.createPlayer(12,0);
+         playerEasy p1 = new playerEasy(bshipboard);
+         p1.createPlayer(20,0);
+  System.out.println("   1 2 3 4 5 6"); 
+
+         bshipboard.showBoard();//Call showBoard method in the battleship board class
+
+         
+        
+         
          
          
       }
-      
+         if (difficulty.equalsIgnoreCase("medium") ) { 
+         
+             
+
+         
+         System.out.println(" 1 2 3 4 5 6 7 8"); 
+ 
+             BattleshipBoard bshipboard = new BattleshipBoard(8,8);
+             PositionofShip p = new PositionofShip(bshipboard) ;
+             p.createShip(3);
+             p.createShip(3);
+
+             
+
+
+
+      System.out.println(" 1 2 3 4 5 6 7 8"); 
+
+             
+             bshipboard.showBoard();
+             playerMedium p2 =  new playerMedium(bshipboard);
+             p2.createPlayer(15,0);
+       System.out.println(" 1 2 3 4 5 6 7 8"); 
+
+             bshipboard.showBoard();//Call showBoard method in the battleship board class
+
+             
+        
+             
+             
+          }
+  if (difficulty.equalsIgnoreCase("hard") ) { 
+         
+             
+
+         
+         
+          System.out.println("   1 2 3 4 5 6 7 8 9 10"); 
+ 
+             BattleshipBoard bshipboard = new BattleshipBoard(10,10);
+             PositionofShip p = new PositionofShip(bshipboard) ;
+             p.createShip(3);
+             p.createShip(2);
+             p.createShip(4);
+
+
+
+             
+       System.out.println("   1 2 3 4 5 6 7 8 9 10"); 
+
+             bshipboard.showBoard();
+             playerHard p3 = new playerHard(bshipboard);
+             p3.createPlayer(11,0);
+        System.out.println("    1 2 3 4 5 6 7 8 9 10"); 
+
+             bshipboard.showBoard();//Call showBoard method in the battleship board class
+
+             
+            
+        
+             
+             
+             
+             
+          }
+  System.out.println("You finished the game! Do you want to play again? "
+   + "Enter Y for yes, or N for no.");
+   goAgain = keyboard.next();
+         }
+         while (goAgain.equalsIgnoreCase("Y")); 
       
    }
-      
-   
+   }
--- Player.java
+++ Player.java
@@ -1,55 +1,47 @@
 import java.util.Scanner;
 
-
 public class Player {
 private static BattleshipBoard board;
 
-
 public Player(BattleshipBoard b){
 board = b;
-
 }
-
 public int createPlayer(int missles, int hits) {
 while (missles > 0 && hits <3) {
+System.out.println("Careful Captain, you only have "+missles+" missiles left!"); //counts how many missiles you have left
 Scanner keyboard = new Scanner(System.in);
-     System.out.println("Please enter a row in which you would like to fire upon. 1 through 8.");
-     int row = keyboard.nextInt();
-     while(row > 8 || row < 1){
-     System.out.println("You dingus! Pick a valid cordinate 1 through 8.");
-     row = keyboard.nextInt();
-     }
+    System.out.println("Please enter a row in which you would like to fire upon. 1 through 8.");
+    int row = keyboard.nextInt();
+    while(row > 8 || row < 1){
+    System.out.println("You dingus! Pick a valid cordinate 1 through 8."); //error checking for rows
+    row = keyboard.nextInt();
+    }
     System.out.println("Please enter a column in which you would like to fire upon. 1 through 8.");
     int col = keyboard.nextInt();
-     while(col > 8 || col < 1 ) {
-     System.out.println("You jamoke! Pick a valid cordinate 1 through 8.");
-     col = keyboard.nextInt();
-     }
-     if(board.getBoard()[row-1][col-1].equals("S"))
-         {
-            
+    while(col > 8 || col < 1 ) {
+    System.out.println("You jamoke! Pick a valid cordinate 1 through 8."); //error checking for columns
+    col = keyboard.nextInt();
+   
+    }
+   
+    if(board.getBoard()[row-1][col-1].equals("S")) //checks if coordinated you guessed are equal to the location of the ship
+        {
+           
             System.out.println("HIT");
-            board.getBoard()[row-1][col-1] = "!";
-           hits++;
-         }
-    
-         else
-         {
+            board.getBoard()[row-1][col-1] = "!"; //ideally, for every hit, the hit would be labeled on the board with an "!", and updated for every turn
+          hits++;
+        }
+   
+        else
+        {
             System.out.println("MISS");
             board.getBoard()[row-1][col-1] = "O";
-         }
-     missles--;
-}
+        }
+    missles--;
+    board.showBoard();
 
-         if(hits < 3)
-            System.out.println("Sorry, but you lost because you didn't sink the ship.");
-         if(missles < 1)
-            System.out.println("You have lost all your torpedos");
-         if(hits > 3)
-            {
-               System.out.println("You have beaten the game battleship, Thanks for playing!");
-            }
-         System.out.println("Good game, well played!");
+}
+         
 return hits;
       }
       
